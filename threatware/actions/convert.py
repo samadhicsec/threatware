212721b4a7979fb4f9275f0dcd07cbec43849c44
threatware <threatware>
2026-02-07 15:11:57 +0000

    Support a docID or a full document

--- threatware/actions/convert.py
+++ threatware/actions/convert.py
@@ -6,10 +6,11 @@ Converts a Threat Model document into a model
 import logging
 from threatware.utils.error import ConvertError, ProviderError
 from threatware.utils.output import FormatOutput
+from threatware.utils.location import Location
+from threatware.schemes.schemes import get_document_storage, get_default_template
 from threatware.convertors import convertors_config
 import threatware.convertors.confluence_convertor.convertor
 import threatware.convertors.gdoc_convertor.convertor
-from threatware.utils.output import FormatOutput
 
 import threatware.utils.logging
 logger = logging.getLogger(threatware.utils.logging.getLoggerName(__name__))
@@ -24,20 +25,22 @@ def _output(config:dict):
     return FormatOutput(config.get("output", {}))
 
 
-def convert(config:dict, execution_env, scheme:dict, doc_location:str, store_doc:bool=True):
+def convert(config:dict, execution_env, scheme:dict, location:Location, store_doc:bool=True):
 
     logger.info("Entering convert")
     
     output = _output(config)
 
+    scheme_doc_storage = get_document_storage(scheme)
+
     try: 
-        if scheme['document-storage'] == "confluence":
-            model = threatware.convertors.confluence_convertor.convertor.convert(config, execution_env.getConfluenceConnectionCredentials(), scheme, {"id":doc_location}, store_doc)
-        elif scheme['document-storage'] == "googledoc":
-            model = threatware.convertors.gdoc_convertor.convertor.convert(config, execution_env.getGoogleCredentials(), scheme, {"id":doc_location}, store_doc)
+        if scheme_doc_storage == "confluence":
+            model = threatware.convertors.confluence_convertor.convertor.convert(config, execution_env.getConfluenceConnectionCredentials(), scheme, location, store_doc)
+        elif scheme_doc_storage == "googledoc":
+            model = threatware.convertors.gdoc_convertor.convertor.convert(config, execution_env.getGoogleCredentials(), scheme, location, store_doc)
         else:
-            logger.error(f"Unknown document storage type '{scheme['document-storage']}'")
-            raise ConvertError("unknown-doc-storage", {"doc_storage":scheme['document-storage']})
+            logger.error(f"Unknown document storage type '{scheme_doc_storage}'")
+            raise ConvertError("unknown-doc-storage", {"doc_storage":scheme_doc_storage})
 
         output.setSuccess("success", {}, model)
 
@@ -49,7 +52,7 @@ def convert(config:dict, execution_env, scheme:dict, doc_location:str, store_doc
     return output
 
 
-def convert_template(config:dict, execution_env, scheme:dict, doc_location:str):
+def convert_template(config:dict, execution_env, scheme:dict, location:Location):
 
     logger.info("Entering convert_template")
 
@@ -57,12 +60,12 @@ def convert_template(config:dict, execution_env, scheme:dict, doc_location:str):
 
     try: 
 
-        if not doc_location:
-            doc_location = scheme['template-id']
-            if not doc_location:
+        if not location.isDocumentProvided() and not location.isLocationIDProvided():
+            template_location = get_default_template(scheme)
+            if not template_location:
                 logger.error("No template document location provided as input or defined in the scheme")
-        
-        return convert(config, execution_env, scheme, doc_location, store_doc=False)
+                raise ConvertError("no-template-doc", {})
+        return convert(config, execution_env, scheme, location, store_doc=False)
     
     except ConvertError as error:
         output.setError(error.text_key, error.template_values)
